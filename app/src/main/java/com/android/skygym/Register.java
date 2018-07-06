302d696b9e4f364950a3f4ec418d1f8a34e4769c
DennisMaigua <denniezy08@gmail.com>
2018-07-06 23:16:59 +0300

    Changes

--- app/src/main/java/com/android/skygym/Register.java
+++ app/src/main/java/com/android/skygym/Register.java
@@ -1,14 +1,23 @@
 package com.android.skygym;
 
+
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Register extends AppCompatActivity {
     private TextView login;
@@ -16,10 +25,13 @@ public class Register extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_register);
+
+        final EditText fname = findViewById(R.id.firstname);
+        final EditText lname = findViewById(R.id.lastname);
+        final EditText eml = findViewById(R.id.email);
+        final EditText pwd = findViewById(R.id.password);
+        final Button bRegister = findViewById(R.id.sign_up_button);
 
         login = findViewById(R.id.login_to_account);
         login.setOnClickListener(new View.OnClickListener() {
@@ -29,6 +41,42 @@ public class Register extends AppCompatActivity {
                 login.setPaintFlags(login.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
                 Intent intent = new Intent(Register.this, Login.class);
                 startActivity(intent);
+            }
+        });
+
+        bRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String first_name = fname.getText().toString();
+                final String last_name = lname.getText().toString();
+                final String email = eml.getText().toString();
+                final String password = pwd.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if (success) {
+                                Intent intent = new Intent(Register.this, Login.class);
+                                Register.this.startActivity(intent);
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(Register.this);
+                                builder.setMessage("Registration failed!")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                RegisterRequest registerRequest = new RegisterRequest(first_name, last_name, email, password, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(Register.this);
+                queue.add(registerRequest);
             }
         });
     }
