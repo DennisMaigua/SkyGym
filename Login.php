a6cc392378ab33f22c9fdf5199b10e74de4a8986
DennisMaigua <32277450+DennisMaigua@users.noreply.github.com>
2018-07-06 14:53:29 +0300

    Update Login.php

--- Login.php
+++ Login.php
@@ -4,7 +4,7 @@
     $email = $_POST["email"];
     $password = $_POST["password"];
     
-    $statement = mysqli_prepare($con, "SELECT * FROM user WHERE email = ? AND password = ?");
+    $statement = mysqli_prepare($con, "SELECT * FROM users WHERE email = ? AND password = ?");
     mysqli_stmt_bind_param($statement, "ss", $email, $password);
     mysqli_stmt_execute($statement);
     
